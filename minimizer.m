bc16242ad167c83dc156e643bd8dae871044718a
maany <imptodefeat@gmail.com>
2016-12-14 12:10:26 +0530

    optimal and of-only solution added

--- minimizer.m
+++ minimizer.m
@@ -4,9 +4,9 @@ PiO = dlmread('cost_of_link.dat');
 PiH = dlmread('cost_rf_fso_link.dat');
 node_names = strsplit(fileread('node_names.txt'),',');
 adjacency_matrix = X + Y;
-planning_graph = graph(node_names,adjacency_matrix);
-#calculate closes nodes
-closest_nodes = 
-# validate using function 
-# calculate weights
+#planning_graph = graph(adjacency_matrix,node_names);
 
+#optimal solution
+printf("optimal planning solution is %d",optimal_planning(X,Y,PiO,PiH));
+#of_only_planning
+printf("of_only planning solution is %d",of_only_planning(X,PiO));
---
+++ of_only_planning.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2016 Mayank Sharma
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {Function File} {@var{retval} =} of_only_planning (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Mayank Sharma <devmaany@devmaany-Lenovo-Y50-70>
+## Created: 2016-12-14
+
+function cost = optimal_planning (X, PiO)
+% This function returns optimal cost of backhaul network
+  val = 0.5*(X.*PiO)
+  val(val==0) = inf;
+  cost= min(min(val));
+endfunction
